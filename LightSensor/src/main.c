a7e35136a0bb6761bd5110165e3ea86a503a1afb
Ben Prisby <bdprisby@gmail.com>
2019-02-22 17:59:57 -0500

    Just output to stdout for the light sensor.

--- LightSensor/src/main.c
+++ LightSensor/src/main.c
@@ -1,18 +1,15 @@
 /** @file main.c
  *  @brief HUDView light sensor control application.
  *
- *  This program initializes and periodically reads the Adafruit TSL2561 light sensor lux value and appends it to the
- *  the specified output pipe for downstream consumption by the control application.
+ *  This program initializes and periodically reads the Adafruit TSL2561 light sensor lux value and prints it to stdout
+ *  for downstream consumption by the control application.
  *
  *  @author Ben Prisby (BenPrisby)
  */
 
-#include <fcntl.h>
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/stat.h>
-#include <sys/types.h>
 #include <unistd.h>
 
 #include "tsl2561.h"
@@ -22,8 +19,6 @@
 /*--------------------------------------------------------------------------------------------------------------------*/
 
 static void *pvSensor = NULL;
-static const char *pcPipe = "/tmp/hudview_light_sensor_output";
-static FILE *pOutput = NULL;
 /*--------------------------------------------------------------------------------------------------------------------*/
 
 static void vSignalHandler( int iSignal );
@@ -39,58 +34,33 @@ int main( int argc, char ** argv )
     /* Install the Ctrl-C handler. */
     signal( SIGINT, vSignalHandler );
 
-    /* Attempt to establish a named pipe for output. */
-    if ( 0 <= mkfifo( pcPipe, 0666 ) )
+    /* Disable buffering on standard output. */
+    setbuf( stdout, NULL );
+
+    /* Attempt initialize the sensor. */
+    pvSensor = tsl2561_init( iSensorAddress, pcBus );
+
+    if ( NULL != pvSensor )
     {
-        /* Attempt to open the pipe for writing. */
-        pOutput = fopen( pcPipe, "r+" );
+        /* Configure initial settings. */
+        tsl2561_enable_autogain( pvSensor );
+        tsl2561_set_integration_time( pvSensor, TSL2561_INTEGRATION_TIME_13MS );
 
-        if ( NULL != pOutput )
+     /* Periodically collect the lux reading and append it to the output file. */
+        for ( ;; )
         {
-            /* Disable buffering on the output file. */
-            setbuf( pOutput, NULL );
-
-            /* Attempt initialize the sensor. */
-            pvSensor = tsl2561_init( iSensorAddress, pcBus );
-
-            if ( NULL != pvSensor )
-            {
-                /* Configure initial settings. */
-                tsl2561_enable_autogain( pvSensor );
-                tsl2561_set_integration_time( pvSensor, TSL2561_INTEGRATION_TIME_13MS );
-
-                /* Periodically collect the lux reading and append it to the output file. */
-                for ( ;; )
-                {
-                    lLuxReading = tsl2561_lux( pvSensor );
-                    fprintf( pOutput, "%lu\n", lLuxReading );
-                    sleep( WAIT_TIME_SECONDS );
-                }
-
-                /* Should never get here. */
-                fclose( pOutput );
-                unlink( pcPipe );
-                tsl2561_close( pvSensor );
-                iReturn = -1;
-            }
-            else
-            {
-                /* Failed to initialize sensor. */
-                fclose( pOutput );
-                unlink( pcPipe );
-                iReturn = -1;
-            }
+            lLuxReading = tsl2561_lux( pvSensor );
+            printf( "%lu\n", lLuxReading );
+            sleep( WAIT_TIME_SECONDS );
         }
-        else
-        {
-            /* Failed to open the pipe for writing. */
-            unlink( pcPipe );
-            iReturn = -1;
-        }
+
+        /* Should never get here. */
+        tsl2561_close( pvSensor );
+        iReturn = -1;
     }
     else
     {
-        /* Failed to create the pipe. */
+        /* Failed to initialize sensor. */
         iReturn = -1;
     }
 
@@ -103,11 +73,8 @@ static void vSignalHandler( int iSignal )
     /* Check for a signal to quit. */
     if ( SIGINT == iSignal )
     {
-        fclose( pOutput );
-        unlink( pcPipe );
         tsl2561_close( pvSensor );
         exit( 0 );
     }
 }
 /*--------------------------------------------------------------------------------------------------------------------*/
-
