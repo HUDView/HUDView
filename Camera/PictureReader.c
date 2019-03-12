b76e460d995a3bbd334cc354081cc0ffdac27ab3
schwartzmanb <schwartzman.ben@gmail.com>
2019-03-12 10:52:50 -0400

    Fix includes and variable name.

--- Camera/PictureReader.c
+++ Camera/PictureReader.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
-#include <"ssd1306_16bit.h">
+//#include <"ssd1306_16bit.h">
 #include <unistd.h>
+#include <string.h>
 
 static const char *cameraOutput = "/tmp/hudview_camera_output";
 // image size = resolution * bytes/pixel
 static const int imageSize = 160 * 120 * (16 / 2);
 // sleep is 1 / framewrate of camera in microseconds
-static const int sleep = 1000000 / 10;
+static const int sleepTime = 1000000 / 10;
 
 int main(int argc, char **argv) {
     FILE* imageFile;
@@ -17,6 +18,6 @@ int main(int argc, char **argv) {
         memset(imageData, 0, imageSize);
         size_t newLen = fread(imageData, sizeof(unsigned char), imageSize, imageFile);
         ssd1306_drawBitmap16(0, 0, 160, 120, &imageData);
-        usleep(sleep);
+        usleep(sleepTime);
     }
 }
